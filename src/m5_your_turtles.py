0d5c422d338444f3daed57dcb8c123d49fa5a266
fernerme <fernerme@users.noreply.github.com>
2020-06-04 17:47:32 -0400

    trash, but could be worse!

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Melina Ferner.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,45 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+
+leftie = rg.SimpleTurtle()
+leftie.pen = rg.Pen('red', 2)
+leftie.speed = 10
+rightie = rg.SimpleTurtle()
+rightie.pen = rg.Pen('blue', 2)
+rightie.speed =10
+
+rightie.pen_up()
+rightie.forward(150)
+rightie.left(90)
+rightie.pen_down()
+
+leftie.pen_up()
+leftie.left(180)
+leftie.forward(150)
+leftie.right(90)
+leftie.pen_down()
+r1 = 1
+r2 = 2
+
+for k in range(20):
+    leftie.draw_circle(r1)
+    leftie.pen_up()
+    leftie.right(90)
+    leftie.forward(20)
+    leftie.left(90)
+    leftie.pen_down()
+    rightie.draw_circle(r2)
+    rightie.pen_up()
+    rightie.right(90)
+    rightie.forward(30)
+    rightie.left(90)
+    rightie.pen_down()
+    r1 = r1+20
+    r2 = r2+30
+
+
+
+window.close_on_mouse_click()
